3378b3680b9894f65225894037f534bd3abf38ff
Caitlin Geier <geier.ac@gmail.com>
2017-12-19 15:43:32 -0500

    Added slide 7 to intro, started "design review" slides

--- src/components/slides/index.js
+++ src/components/slides/index.js
@@ -8,6 +8,8 @@ import multiExample1 from '../../img/multiselect-combobox-example-1.png';
 import multiExample2 from '../../img/multiselect-combobox-example-2.png';
 import singleExample1 from '../../img/single-select-combobox-example-1.png';
 import singleExample2 from '../../img/single-select-combobox-example-2.png';
+import teamwork from '../../img/teamwork.png';
+import codevdesign from '../../img/code-vs-design.png';
 
 // NOTE: for code blocks use --> import Highlight from 'react-highlight';
 
@@ -44,8 +46,8 @@ const slides = [
   ),
   (
     <div>
-      <h2>What We Do</h2>
-      <p>We work for <strong>Deque Systems</strong>.</p>
+      <h2 className="large">What We Do</h2>
+      <h3>We work for <strong>Deque Systems</strong>.</h3>
       &nbsp;
       <p>We design and build <strong>web applications</strong> to help <strong>development teams</strong> perform <strong>accessibility testing</strong>.</p>
     </div>
@@ -65,7 +67,7 @@ const slides = [
     <div>
       <h2 className="title-centered">Framing the Problem</h2>
       <div className="full-image">
-        <img src={oldFilters} width="400px;" alt="multi-select filters in WorldSpace Assure prior to version 1.4" />
+        <img src={oldFilters} width="400px" alt="multi-select filters in WorldSpace Assure prior to version 1.4" />
       </div>
     </div>
   ),
@@ -73,12 +75,39 @@ const slides = [
     <div>
       <h2 className="title-offscreen">Framing the Problem: Existing Comboboxes</h2>
       <div className="two-col">
-        <img src={singleExample1} />
-        <img src={multiExample2} />
+        <img src={singleExample1} alt="single select combobox example 1"/>
+        <img src={multiExample2} alt="multi select combobox example 1"/>
       </div>
       <div className="two-col">
-        <img src={multiExample1} />
-        <img src={singleExample2} />
+        <img src={multiExample1} alt="multi select combobox example 2"/>
+        <img src={singleExample2} alt="single select combobox example 2"/>
+      </div>
+    </div>
+  ),
+  (
+    <div>
+      <h2 className="title-centered large">Accessibility === difficult</h2>
+      <h3 className="title-centered image-space">Working with teammates === less difficult</h3>
+      <div className="full-image">
+        <img className="bottom-aligned-image" src={teamwork} width="500px" alt="" />
+      </div>
+    </div>
+  ),
+  (
+    <div>
+      <h1 className="title-centered">Design Reviews</h1>
+    </div>
+  ),
+  (
+    <div>
+      <div className="background-image">
+        <img src={codevdesign} alt="" width="600px"/>
+      </div>
+      <div className="two-col">
+        <h2 className="align-right">Designer</h2>
+      </div>
+      <div className="two-col">
+        <h2 className="align-left white-text">Developer</h2>
       </div>
     </div>
   )
